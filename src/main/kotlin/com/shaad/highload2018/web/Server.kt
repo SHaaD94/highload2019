0f9422c798625e71f92bff084d931b51b8253f59
Evgeniy <eugenzuy@gmail.com>
2019-01-14 08:12:32 +0700

    1

--- src/main/kotlin/com/shaad/highload2018/web/Server.kt
+++ src/main/kotlin/com/shaad/highload2018/web/Server.kt
@@ -32,28 +32,33 @@ class Server @Inject constructor(
 
 
     override fun handle(ctx: Channel, buf: Buf, data: RapidoidHelper): HttpStatus {
-        val method = buf.get(data.verb)
-        return method2Handler[method]
-            ?.firstOrNull { it.matches(buf, data.path) }
-            ?.let { handler ->
-                kotlin.runCatching { handler.process(buf, data.path, data.query, data.body) }
-                    .onSuccess {
-                        startResponse(ctx, it.code, true)
-                        writeBody(ctx, it.body, 0, it.body.size, json)
-                    }
-                    .onFailure { error ->
-                        error.printStackTrace()
-                        startResponse(ctx, 500, true)
-                        writeBody(
-                            ctx,
-                            error.message?.toByteArray() ?: "Internal server error".toByteArray(),
-                            0,
-                            error.message?.toByteArray()?.size ?: 21,
-                            json
-                        )
-                    }.getOrNull()
-                HttpStatus.DONE
-            }
-            ?: HttpStatus.NOT_FOUND
+        return try {
+            val method = buf.get(data.verb)
+            method2Handler[method]
+                ?.firstOrNull { it.matches(buf, data.path) }
+                ?.let { handler ->
+                    kotlin.runCatching { handler.process(buf, data.path, data.query, data.body) }
+                        .onSuccess {
+                            startResponse(ctx, it.code, true)
+                            writeBody(ctx, it.body, 0, it.body.size, json)
+                        }
+                        .onFailure { error ->
+                            error.printStackTrace()
+                            startResponse(ctx, 500, true)
+                            writeBody(
+                                ctx,
+                                error.message?.toByteArray() ?: "Internal server error".toByteArray(),
+                                0,
+                                error.message?.toByteArray()?.size ?: 21,
+                                json
+                            )
+                        }.getOrNull()
+                    HttpStatus.DONE
+                }
+                ?: HttpStatus.NOT_FOUND
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            HttpStatus.ERROR
+        }
     }
 }
